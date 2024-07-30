664bf3cf0f1eec504258454b26e6fe409fef139f
myy <18599141728@163.com>
2024-07-30 16:01:04 +0800

    添加接口路径

--- src/main/java/com/myy/up/Controller/UserController.java
+++ src/main/java/com/myy/up/Controller/UserController.java
@@ -4,11 +4,13 @@ import com.myy.up.Service.UsersService;
 import com.myy.up.Vo.UsersVo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("user/")
 public class UserController {
 
     private final UsersService usersService;
